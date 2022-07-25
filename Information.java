8d629041858fef821e45acb6db697dac7cf2d5d1
Pooja4120 <109073532+Pooja4120@users.noreply.github.com>
2022-07-25 12:58:21 +0530

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,34 @@
+public class Information
+{
+    public static void main (String[]args)
+    {    
+    String Name = "Puja Bhandari";
+    String Address = "Green Valley Jaysingpur";
+	String PhoneNo = "8459162955";
+	String Gender = "Female";
+	String DOB = "04/01/2000";
+	float Height = 152.4f ;
+	float weight = 40.5f;
+	int id = 12345678;
+	String  Degree = "btech" ;
+	short year = 2022;
+	double GPA = 80.4;
+	boolean tuition = true ;
+	boolean scholar = true ;
+	System.out.println("Information About Students : ");
+	System.out.println("Name of the Student : " + Name);
+	System.out.println("Address of the Student : " + Address);
+	System.out.println("Phone Number of the Student : " + PhoneNo);
+	System.out.println("Gender of the Student : " + Gender);
+	System.out.println("Date of Birth of the Student : " + DOB);
+	System.out.println("Height of the student : " + Height);
+	System.out.println("weight of the Student : " + weight);
+	System.out.println(" unique id of student : " +id);
+	System.out.println("Pursued Degree : " + Degree);
+	System.out.println("Year of Study : " + year);
+	System.out.println("Average GPA : " + GPA);
+	System.out.println("Tuition Grant : " + tuition);
+	System.out.println(" scholar : " + scholar);
+
+    }
+}
