b17df9a680ec19ab24634530c711a8dd0452d466
Pooja4120 <109073532+Pooja4120@users.noreply.github.com>
2022-07-25 13:10:57 +0530

    Add files via upload

---
+++ Positiveornegative.java
@@ -0,0 +1,16 @@
+public class Positiveornegative
+{
+public static void main(String[]args)
+{
+int i=13;
+int num=i;
+if(i >0)
+{
+System.out.println("the number is positive");
+}
+else if(i <0)
+{
+System.out.println("the number is negative");
+}
+}
+}
